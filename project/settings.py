4141cfb9ab3f1b0403ad01555d1fcbce6e62800b
Malathi <malathi.t2002@gmail.com>
2025-02-14 14:51:13 +0530

    Initial Commit

--- project/settings.py
+++ project/settings.py
@@ -122,7 +122,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'malathi.t2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'lvjb wzip mwop nhmi'
+EMAIL_HOST_PASSWORD = '1234578@@'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
